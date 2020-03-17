e5244a03e7616125dc56bf5fb6a551b811e96041
Chanda Hubbard <56906759+ChandaHubbard@users.noreply.github.com>
2020-03-17 13:54:57 -0700

    Add files via upload

---
+++ src/AddFolder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class AddFolder extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <label htmlFor="name">Name:</label>
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddFolder;
